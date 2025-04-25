37a4b41e118c44754e3a36539fb4527b016e59a8
lisagiri <mjrey1123@gmail.com>
2025-04-25 14:30:20 +0800

    Initial commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2109760.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 203370048, 204437282, 207846731, 207884146, 207884149, 207884161, 207885260, 207888825, 207890341, 207890962, 207891167, 207891346, 207891476, 207891477, 207891593, 207891595, 207891637, 207891644, 207891649, 207891651, 207891728, 207894608, 207895518, 207895524, 207895525, 207895526, 207895777, 207897208, 207911868 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/203399567/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/203399568/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/203399566/1/ammo.js', 'preload' : true},
+];
